f217d5cdc16fb391c445bae7d9fa1460b76d38f4
that is MTZ <timamakhmutovz@gmail.com>
2023-04-03 10:51:43 +0300

    home task ch8

--- task_3/solution.js
+++ task_3/solution.js
@@ -4,12 +4,12 @@ function renderCartItem(item) {
     // Задание №3.1. Формирование строки корзины
 
     let itemCountText = "";
-        itemCountText = itemCountText + product["count"];
+        itemCountText = itemCountText + product.count;
         itemCountText = itemCountText + " x ";
-        itemCountText = itemCountText + product["price"];
+        itemCountText = itemCountText + product.price;
         itemCountText = itemCountText + " ₽ = ";
         
-    let sum = product["count"] * product["price"];
+    let sum = product.count * product.price;
     
         itemCountText = itemCountText + sum;
         itemCountText = itemCountText + " ₽";
