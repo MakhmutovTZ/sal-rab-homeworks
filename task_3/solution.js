b15421a51ce7f2b50a441d526ccaceca740f6784
that is MTZ <timamakhmutovz@gmail.com>
2023-04-03 12:12:42 +0300

    home task ch12

--- task_3/solution.js
+++ task_3/solution.js
@@ -3,16 +3,7 @@ function renderCartItem(item) {
 
     // Задание №3.1. Формирование строки корзины
 
-    let itemCountText = ``;
-        itemCountText = itemCountText + product.count;
-        itemCountText = `${itemCountText} x `;
-        itemCountText = itemCountText + product.price;
-        itemCountText = `${itemCountText} ₽ = `;
-        
-        let sum = product.count * product.price;
-    
-        itemCountText = itemCountText + sum;
-        itemCountText = `${itemCountText} ₽`;
+    let itemCountText = `${product.count} × ${product.price} ₽ = ${product.count * product.price} ₽`
   
     // Конец решения задания №3.1.
 
