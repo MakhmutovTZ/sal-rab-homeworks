6026547fde1f9a58c537993de8b217b93b88c172
that is MTZ <timamakhmutovz@gmail.com>
2023-04-07 16:32:01 +0300

    home task ch1

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,15 +1,17 @@
 // Задание 4.1. Вывод карточек товаров
 
 function parseProducts(json) {
-    // Аргументом функции является JSON
-    let data = JSON.parse(json);
-    let products = data.products;
+    let data; 
+        data = JSON.parse(json);
+    let products; 
+        products = data.products;
     return parseProducts(products);
 }
 
 function renderProductsCards(json){
     clearProducts();
-    let products = parseProducts(json);
+    let products; 
+        products = parseProducts(json);
     let length = products.length;
     for (let i = 0; i < length; i += 1) {
         addProduct(products[i]);
