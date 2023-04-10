34603cb5851b706c4f99084a0cc15af35d093d86
that is MTZ <timamakhmutovz@gmail.com>
2023-04-10 15:48:12 +0300

    home task ch01

--- task_final/solution.js
+++ task_final/solution.js
@@ -3,13 +3,13 @@ function sendRequest(name, phone, address, goods, sum) {
     let data = {
         client: name + " " + phone, 
         goods: [], 
-        order: {address: "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв." + address.flat, 
+        order: {address: "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв. " + address.flat, 
         sum: sum},};
-        for (let i = 0; i <= goods.length; i ++) {
-            data.goods.push(goods.Object[i].title, goods.Object[i].count);
+        for (let i = 0; i < goods.length; i ++) {
+            data.goods.push({"title": goods.[i].title, "count": goods.[i].count});
         }
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
