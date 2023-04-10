c8a91db2046eb084d4d5aefd784cc3228bb99a05
that is MTZ <timamakhmutovz@gmail.com>
2023-04-10 11:54:39 +0300

    home task ch yaustalotetogoderma

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,12 +1,12 @@
+
 function sendRequest(name, phone, address, goods, sum) {
-    
     let data = {
         client: name + " " + phone, 
         goods: [], 
         order: {address: "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв." + address.flat, 
         sum: sum},};
         for (let i = 0; i <= goods.length; i ++) {
-            data.goods.push(goods.indexOf(i).title, goods.indexOf(i).count);
+            data.goods.push(goods.Object[i].title, goods.Object[i].count);
         }
 
     let jsonData = JSON.stringify(data);
